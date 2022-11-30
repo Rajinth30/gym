92cb1768e2545b32cc396eb40ccad78e73757887
Rajinth <rajinth3@gmail.com>
2022-11-30 11:27:17 +0530

    Gym

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Box, Typography, Button } from "@mui/material";
+import HeroBannerImage from "../assets/images/exer.jpg";
+
+const HeroBanner = () => {
+  return (
+    <Box
+      sx={{ mt: { lg: "150px", xs: "70px" }, ml: { sm: "50px" } }}
+      position="relative"
+      p="20px"
+
+    >
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        Elite Fitness
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "40px", xs: "36px" } }}
+        mb="23px"
+        mt="30px"
+      >
+        Sweat, Smile <br />
+        And Repeat
+      </Typography>
+      <Typography
+        fontSize="22px"
+        fontFamily="Alegreya"
+        lineHeight="35px"
+        mb={4}
+      >
+        Check out the most effective exercises personalized to you
+      </Typography>
+
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: "#ff2625", padding: "10px" }}
+      >
+        Explore Exercises
+      </Button>
+
+      <Typography
+        fontWeight={600}
+        color="#FF2625"
+        sx={{
+          opacity: "0.1",
+          display: { lg: "block", xs: "none" },
+          fontSize: "200px",
+        }}
+      >
+        Exercise
+      </Typography>
+
+      <img
+        src={HeroBannerImage}
+        alt="hero-banner"
+        className="hero-banner-img"
+      />
+    </Box>
+  );
+};
+
+export default HeroBanner;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,21 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "ab0e983fb4mshe39a2ccaa5abc5cp16853ejsn7973691ca4fa",
+  },
+};
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": "ab0e983fb4mshe39a2ccaa5abc5cp16853ejsn7973691ca4fa",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
